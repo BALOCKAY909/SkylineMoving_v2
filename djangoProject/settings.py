7420a7ede7d717ef653e49ad64b865f10cd21af5
BALOCKAYY <123591627+BALOCKAYY@users.noreply.github.com>
2026-07-28 21:01:29 -0500

    Small change

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -140,5 +140,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')  # Must be a Gmail App Password, NOT your Gmail login password
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
